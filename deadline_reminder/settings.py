01e7803c0ffa9bd0b31951c10baab76220d46a35
Alex Rotaru <alexrotaru1595@gmail.com>
2022-03-19 15:35:23 +0200

    add users app

--- deadline_reminder/settings.py
+++ deadline_reminder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_ee6e!_op8k4b2!3$dl8g+b)_3h1x-^i!pcq@!y8yz%bw82iv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'phonenumber_field',
     'reminders.apps.RemindersConfig',
     'django.contrib.admin',
--- deadline_reminder/urls.py
+++ deadline_reminder/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('reminders.urls')),
+    path('', include('users.urls')),
+    path('test/', include('reminders.urls')),
     path('admin/', admin.site.urls),
 ]
